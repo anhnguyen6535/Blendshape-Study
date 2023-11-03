9631ae9b2d043cb46f2d89a6132c62e016e55ca7
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-11-03 01:39:24 -0600

    Set distance dynamic trigger

--- Assets/Scripts/ConditionController.cs
+++ Assets/Scripts/ConditionController.cs
@@ -7,31 +7,28 @@ public class ConditionController : MonoBehaviour
 {
     public bool isShortDistance = true;
     public bool isOnlyHeadMovement = true;
-    public bool start = false;
-    public string fileName = "blendshapes/airpollution_actor";
-    private AudioSource audioSource;
 
     [SerializeField] private GameObject[] shortDistance;
     [SerializeField] private GameObject[] longDistance;
-    [SerializeField] private FaceActor actor;
-
+    [SerializeField] private GameObject xrCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        conditionTrigger();
-        audioSource = gameObject.GetComponent<AudioSource>();
 
+        // make sure that the first element in each array is user's chair
+        if(isShortDistance){
+            xrCamera.transform.position = shortDistance[0].transform.position;
+        }else{
+            xrCamera.transform.position = longDistance[0].transform.position;
+        }
+        conditionTrigger();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(start){
-            actor.triggerPlay(fileName);
-            GetComponent<LoadData>().triggerPlay();
-            start = false;
-        }
+        
     }
 
     void conditionTrigger(){
